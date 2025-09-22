e7104393375c47553e7d40519dedbf61544dccd4
Pierogabulle <u20231E145@upc.edu.pe>
2025-09-22 17:36:59 -0500

    Añadidos endpoints para búsqueda de Teachers por cursos, campuses y carreras.

--- src/main/java/com/upc/cicloestrella/interfaces/services/TeacherServiceInterface.java
+++ src/main/java/com/upc/cicloestrella/interfaces/services/TeacherServiceInterface.java
@@ -2,6 +2,7 @@ package com.upc.cicloestrella.interfaces.services;
 
 import com.upc.cicloestrella.DTOs.requests.TeacherRequestDTO;
 import com.upc.cicloestrella.DTOs.responses.TeacherResponseDTO;
+import com.upc.cicloestrella.entities.Teacher;
 
 import java.util.List;
 
@@ -11,4 +12,7 @@ public interface TeacherServiceInterface {
     public TeacherResponseDTO show(Long id);
     public TeacherResponseDTO update(Long id, TeacherRequestDTO teacher);
     public void delete(Long id);
+    public List<Teacher> searchByCampuses(String campuses);
+    public List<Teacher> searchByCourses(String courses);
+    public List<Teacher> searchByCareers(String careers);
 }
